4fa55b5261e38b1ef9acc143585dfd4b39b5541f
matthew <matt.elia@gramercytech.com>
2016-01-30 03:28:25 -0500

    models

---
+++ app/FrequentedLocation.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FrequentedLocation extends Model {
+
+	protected $table = 'frequented_locations';
+
+}
---
+++ app/Workplace.php
@@ -0,0 +1,9 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Workplace extends Model {
+
+	protected $table = 'workplaces';
+
+}
