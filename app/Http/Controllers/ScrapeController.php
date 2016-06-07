62efafce546de8635be8711c15e0fa54ac5d406d
matthew-elia <matt.elia@gramercytech.com>
2016-06-07 16:47:19 -0400

    isolate data scrape to relevant listings

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -4,22 +4,41 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Symfony\Component\DomCrawler\Crawler;
 use Goutte\Client;
 
 class ScrapeController extends Controller {
 
 	public function get_scrape_data () {
-		$client = new Client();
-		$priceArray = [];
-	 	for ($i=1000; $i < 1080; $i++) { 
-			$crawler = $client->request('GET', 'http://nymag.streeteasy.com/nyc/building/'+$i+'');
-			$url = $crawler->filter('tbody > tr > td')->eq($i)->first();
-			array_push($priceArray, $url);
-		}
-		
-		dump($priceArray);	
-		
-		
-	}
 
+	$client = new Client();
+	$crawler = new Crawler();
+	
+	$listing_id;
+	$listing_status;
+	$listing_details;
+
+	$nodeValues = [];
+
+	for ($i=1; $i < 250; $i++) { 
+
+		$crawler = $client->request('GET', 'http://nymag.streeteasy.com/nyc/building/'+$i+'');
+		
+			$nodeValues = $crawler->filter('.building-pages > tbody > tr')->each(function ($node, $i) {
+				$listing_id[$i] = $node->attr("id");
+				$listing_status[$i] = $node->attr("data-gtm-track");
+				$listing_details[$i] = $node->children()->first()->html();
+
+				// if($listing_status[$i] !== 'active-sales'){
+				// }
+				
+				dump($i.'|'.$listing_id[$i].': '.$listing_status[$i].'--'.$listing_details[$i]);
+
+			});
+
+	  }
+
+  }
 }
+
+
